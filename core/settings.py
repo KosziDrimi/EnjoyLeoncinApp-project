db09b13dcceb5398068c2892feb437e95e4feaaf
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2021-11-16 12:44:24 +0100

    debug changed to false

--- core/settings.py
+++ core/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rezerwacje-nadwislanin.herokuapp.com']
 
+# DJANGO_SETTINGS_MODULE = config('DJANGO_SETTINGS_MODULE')
 
 # Application definition
 

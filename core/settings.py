3fcbfcaf97c8b461c3052da0af37c0fa8415fcfa
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2021-11-16 11:06:17 +0100

    mail settings changed

--- core/settings.py
+++ core/settings.py
@@ -135,11 +135,11 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'rezerwacjenadwislanin@gmail.com'
 # EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 

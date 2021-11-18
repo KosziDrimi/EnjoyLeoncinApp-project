93e73d0418e65da686c3e964480137737f261379
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2021-11-18 15:09:14 +0100

    settings devided for prod & dev

--- core/prod_settings.py
+++ core/prod_settings.py
@@ -13,24 +13,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['rezerwacje-nadwislanin.herokuapp.com', 'localhost', '127.0.0.1']
 
-# DJANGO_SETTINGS_MODULE = config('DJANGO_SETTINGS_MODULE')
 
 # Application definition
 
@@ -74,17 +67,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
